6e81383c709da8d7e81705e325db7ef80b4c2610
Sicryption <sicryption@gmail.com>
2020-03-26 18:05:28 -0400

    Duplicate Declaration Fix

--- source/sandbox.cpp
+++ source/sandbox.cpp
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern "C"{
+extern "C"
+{
     #include "lua/lua.h"
     #include "lua/lualib.h"
     #include "lua/lauxlib.h"
 }
+
 
 /*  Lua Sandbox object
 *   This object will contain the variables and functions 
@@ -30,7 +32,6 @@ class luaSandbox
 
 
     public:
-
     luaSandbox()
     {
         state = luaL_newstate();
@@ -41,21 +42,12 @@ class luaSandbox
 
     unsigned long int setMemCapacity();
     unsigned long int setMemSize();
-    lua_State * state;
-    //  Binds API functions to the lua state
-    void bindAPI();
-
-    public:
-    //  Defined maximum byte size of the Lua memory block
-    long int memCapacity;
-    //  Current byte size of the Lua memory block
-    long int memSize;
 
     //  Read a global variable from the Lua environment
     void getGlobal(const char* name, int value);
     void getGlobal(const char* name, double value);
     void getGlobal(const char* name, char* value);
-  
+
     //  Write a global variable from the Lua environment
     void setGlobal(const char* name, int value);
     void setGlobal(const char* name, double value);
@@ -64,7 +56,6 @@ class luaSandbox
     //  Execute Lua code
     void execute(const char* code);
     void execute(FILE * fp);
-
 }luaSandbox;
 
 //  TODO: track memory block size
