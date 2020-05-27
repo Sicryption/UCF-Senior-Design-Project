9bb95c6fbd36f001867db72da6cb03cd85c8e6be
Chevron Allen <chevronallen@yahoo.com>
2020-05-27 13:53:54 -0400

    Update hardwareAPI.cpp

---
+++ source/UserAPI/hardwareAPI.cpp
@@ -0,0 +1,10 @@
+/**
+ *  @file This file contains the implementation for user API functions which interact with system hardware
+ *  @brief Hardware interaction API functions
+ */
+#include <3ds.h>
+
+void* UserAPI::micToBuffer(int time)
+{
+
+}
