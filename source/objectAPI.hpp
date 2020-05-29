c665ddb79f329bd782b07d90944a474027e3098a
Kenneth <kjrosario9@gmail.com>
2020-05-29 03:53:30 -0400

    objectAPI init and method init

---
+++ source/objectAPI.hpp
@@ -0,0 +1,16 @@
+#include <citro2d.h>
+#include <m3dia.hpp>
+#include "lua/lua.hpp"
+
+class objectAPI {
+    public:
+    // Our Object Instantiation methods are below:
+    int rectangle(lua_State *L);
+    int circle(lua_State *L);
+    int triangle(lua_State *l);
+
+    //Our Object Manipulation Methods are below:
+
+
+
+};
