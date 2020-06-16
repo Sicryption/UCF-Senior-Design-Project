88102b2ae2b3d82e25c03c6016620bbe8e1990bd
Kenneth <kjrosario9@gmail.com>
2020-06-16 13:52:06 -0400

    init mockup pongPaddle Object

--- source/gameObjects/pongPaddle.cpp
+++ source/gameObjects/pongPaddle.cpp
@@ -1,11 +1,34 @@
 #include "../gameObject.hpp"
+#include "../gameManager.hpp"
 
 class PongPaddle : public GameObject
 {
 
 private:
+    m3d::Color *color;
+    m3d::Rectangle *PongRec;
 
 public:
+    PongPaddle(double _x, double _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    void initialize() {
+        color = new m3d::Color(0,100,0);
+        PongRec = new m3d::Rectangle((int)x,(int)y,50,10,*color);
+    }
+
+    void update() {
+
+
+    }
+
+    void draw() {
+        m3d::Screen *screen = GameManager::getScreen();
+        screen->drawTop(*PongRec);
+    }
 
 
 };
