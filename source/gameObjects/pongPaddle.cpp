12c301e6493d0df3ff47e947555ac5c10777916f
Kenneth <kjrosario9@gmail.com>
2020-06-18 18:14:40 -0400

    pongpaddle movement changes

--- source/gameObjects/pongPaddle.cpp
+++ source/gameObjects/pongPaddle.cpp
@@ -7,6 +7,7 @@ class PongPaddle : public GameObject
 private:
     m3d::Color *color;
     m3d::Rectangle *PongRec;
+    m3d::BoundingBox *paddleBox;
 
 public:
     PongPaddle(double _x, double _y)
@@ -18,10 +19,18 @@ public:
     void initialize() {
         color = new m3d::Color(0,100,0);
         PongRec = new m3d::Rectangle((int)x,(int)y,50,10,*color);
+        paddlebox = PongRec->getBoundingBox();
     }
 
     void update() {
 
+        if(m3d::buttons::buttonDown(m3d::buttons::Button::Down)) {
+            moveTo(x, 1 * GameManager::getDeltaTime());
+        }
+
+        if(m3d::buttons::buttonDown(m3d::buttons::Button::Up)) {
+            moveTo(x, -1 * GameManager::getDeltaTime());
+        }
 
     }
 
@@ -29,6 +38,11 @@ public:
         m3d::Screen *screen = GameManager::getScreen();
         screen->drawTop(*PongRec);
     }
+
+    void moveTo(double _x, double _y) {
+        PongRec->setYPosition(y + (int)(_y + 0.5));
+    }
+    
 
 
 };
