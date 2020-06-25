732ad8f5b0fa59fb850ebd7067b957e8ce5ed145
Chevron Allen <chevronallen@yahoo.com>
2020-06-25 07:42:08 -0400

    objectAPI, Scene, and minor command changes

--- source/UserAPI/objectAPI.cpp
+++ source/UserAPI/objectAPI.cpp
@@ -4,6 +4,7 @@
 #include "../userAPI.h"
 #include "../util.hpp"   
 #include "../m3diaLibCI/text.hpp"
+#include "../sceneManager.hpp"
 
 #define DEFAULT_SIZE 10 
 #define DEFAULT_COLOR m3d::Color(255,0,0)
@@ -12,12 +13,33 @@
 
 int UserAPI::move(lua_State* L)
 {
+    std::string t_id = lua_tostring(L,-1);
+    lua_Number x = lua_tonumber(L,-2);
+    lua_Number y = lua_tonumber(L,-3);
+
+    Scene* scene = SceneManager::getScene();
+    if(scene == nullptr)
+    {
+        Util::PrintLine("Error: no current scene");
+        return 0;
+    }
+    GameObject* obj = scene->findObject(t_id);
+    if(obj == nullptr)
+    {
+        Util::PrintLine("Error: couldnt find object \'" + t_id +"\' in Scene \'" + scene->getSceneName() + "\'");
+        return 0;
+    }
+    
+    obj->moveTo(x,y);
+
+
     return 0;
 }
 
 int make_rectangle(lua_State* L)
 {
-    return 0;
+    
+    return 1;
 }
 
 int make_circle(lua_State* L) 
