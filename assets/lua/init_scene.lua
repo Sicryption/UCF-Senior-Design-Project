02800b4f1f69ea60126fec7fa1ca542c0f50688c
Chevron Allen <chevronallen@yahoo.com>
2020-06-25 21:40:09 -0400

    Update init_scene.lua

--- assets/lua/init_scene.lua
+++ assets/lua/init_scene.lua
@@ -6,18 +6,7 @@ function tick()
 end
 
 function select_gameobject(name)
-    local value = name_table[name]
-
-    if (type(value) != "table") then 
-        error("cannot select non-gameobject '"..value.."'") 
-        return nil 
-    end
-
-    if(type(value["id"])= "number" ) then
-        current_object = value["id"] or 0
-    end
-
-    return current_object
+    return  value = name_table[name]
 end
 
 function delete(name)
