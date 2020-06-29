be07927c0124d342c846d1ea5a2c807eee1a01d4
garuperto <46684759+garuperto@users.noreply.github.com>
2020-06-29 11:34:18 -0400

    Maze Adjustments

--- source/gameObjects/mazePlayer.hpp
+++ source/gameObjects/mazePlayer.hpp
@@ -8,15 +8,16 @@ class TerminalObject : public GameObject
 private:
     m3dCI::Sprite *texture;
     m3dCI::Sprite *sprite;
-    bool (*wall)[24][40];
+    bool (*wall)[12][20];
+    int counter;
     //pointer to maze array 
 
 public:
-    TerminalObject(bool (*walls)[24][40])
+    TerminalObject(bool (*walls)[12][20])
     {
         wall =  walls;
-        x = 10;
-        y = 0;
+        x = 20;
+        y = 20;
         xScale = 1;
         yScale = 1;
         angle = 0;
@@ -51,11 +52,60 @@ public:
     {
         if( (x + _x) < 0 || (y + _y) < 0 || (x + _x) >= 40 || (y + _y) >= 24)
             return;
+        /*
         if((*wall)[(int)(x + _x)][(int)(y + _y)]==0)
         {
             x += (_x * 10);
 	        y += (_y * 10);
+        }*/
+        if(_y == 0)
+        {
+            if(_x > 0)
+            {
+                _x += x;
+                for(int i = x; i < (int)_x; i++)
+                {
+                    if((*wall)[i][y] == 1)
+                        break;
+                    x += 10;
+                }
+            }
+            if(_x < 0)
+            {
+                _x += x;
+                for(int i = x; i > (int)_x; i--)
+                {
+                    if((*wall)[i][y] == 1)
+                        break;
+                    x -= 10;
+                }
+            }
+            
         }
+        if(_x==0)
+        {
+            if(_y > 0)
+            {
+                _y += y;
+                for(int i = y; i < (int)_y; i++)
+                {
+                    if((*wall)[x][i] == 1)
+                        break;
+                    y += 10;
+                }
+            }
+            if(_y < 0)
+            {
+                _y += y;
+                for(int i = y; i > (int)_y; i--)
+                {
+                    if((*wall)[x][i] == 1)
+                        break;
+                    y -= 10;
+                }
+            }
+        }
+        
     };
     void Rotate(double deg){};
 };
