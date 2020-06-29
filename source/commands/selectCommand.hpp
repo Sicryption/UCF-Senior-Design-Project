b051a52faca2c027e8191df5ddfdd7074417abd3
Sicryption <sicryption@gmail.com>
2020-06-29 13:16:13 -0400

    Fixed Text Object Rendering for CommandObjects

--- source/commands/selectCommand.hpp
+++ source/commands/selectCommand.hpp
@@ -8,7 +8,7 @@ public:
     SelectCommand(std::string t_param = "object",bool t_lockEdit=false,bool t_lockAdd=false): CommandObject(t_lockEdit, t_lockAdd)
     {
         m_name="select";
-        m_params[0] = t_param;
+		setParam(0, t_param);
     }
 
     ~SelectCommand();
