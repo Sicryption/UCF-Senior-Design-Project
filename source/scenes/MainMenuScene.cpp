14eefba39f9e3c86c34a256227610367dd87fbe5
Sicryption <sicryption@gmail.com>
2020-07-21 10:37:34 -0400

    IT WAS HERE ALL ALONG

--- source/scenes/MainMenuScene.cpp
+++ source/scenes/MainMenuScene.cpp
@@ -21,6 +21,7 @@ MainMenuScene::MainMenuScene()
 	menu->AddItem(ClickHereToContinue);
 
 	apple = new SpriteMenuItem(*ResourceManager::getSprite("apple.png"));
+	apple->setScale(0.35f, 0.35f);
 	menu->AddItem(apple);
 
 	grass = new SpriteMenuItem(*ResourceManager::getSprite("grassB1.png"));
