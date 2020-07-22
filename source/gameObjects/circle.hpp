70fa2fdb1bb48da1800e1664e2d081f509394532
Sicryption <sicryption@gmail.com>
2020-07-22 17:45:20 -0400

    Implement Functions for Rect/Circle/Text

--- source/gameObjects/circle.hpp
+++ source/gameObjects/circle.hpp
@@ -36,10 +36,12 @@ public:
     {
         x = t_x;
         y = t_y;
+		color = t_color;
+		radius = t_radius;
         xScale = t_radius;
         yScale = t_radius;
         angle = t_angle;
-        circle = new m3d::Circle(x,y,xScale,color);
+        circle = new m3d::Circle(x,y,radius,color);
     }
 
     ~CircleObject()
@@ -86,5 +88,39 @@ public:
 	BoundingBox getAABB()
 	{
 		return m3d::BoundingBox(x, y, radius * 2, radius * 2);
+	}
+
+	/**
+		@brief Overrides the GameObject SetPosition function.
+		Keeps the paddle within screen bounds.
+	*/
+	void setPosition(double t_x, double t_y)
+	{
+		x = t_x;
+		y = t_y;
+	}
+
+	/**
+		@brief Implement the GameObject setScale function.
+		@param t_x X Scale
+		@param t_y Y Scale
+	*/
+	void setScale(double t_x, double t_y)
+	{
+		xScale = t_x;
+		yScale = t_y;
+
+		//Can't change X and Y scales. Would need changed via m3d::Circle
+		circle->setRadius(t_x * radius);
+	}
+
+	/**
+		@brief Implement the GameObject setColor function.
+		@param t_color Color to change to
+	*/
+	void setColor(m3d::Color t_color)
+	{
+		m_color = t_color;
+		circle->setColor(t_color);
 	}
 };
